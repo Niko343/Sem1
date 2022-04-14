c4e9ac3ca6d2d7d4e3cb66c8ea48e256b65fc15b
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-14 12:51:24 +0300

    Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+
+
+if(a > b)
+{
+   if(a > c)
+   {
+        Console.Write("Наибольшее число: ");
+        Console.Write(a);
+   }
+   else
+   {
+        Console.Write("Наибольшее число: ");
+        Console.Write(c);
+   }
+} 
+else
+{ 
+    if(b > c)
+    {
+        Console.Write("Наибольшее число: ");
+        Console.Write(b); 
+    }
+    else
+   {
+        Console.Write("Наибольшее число: ");
+        Console.Write(c);
+   }
+}
