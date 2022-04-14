1e4dafdc68ed55436b036a07f32108153b40b0dc
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-14 11:40:29 +0300

    Pervoe zadanie

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if(b == a*a) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
