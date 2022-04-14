3af7b3f14449067184a7919160fbf76d859961a7
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-14 12:11:54 +0300

    Ex7

--- Ex7/Program.cs
+++ Ex7/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a*(-1);
+while (b <= a);
+{
+    Console.WriteLine(b);
+    Console.WriteLine(" ");
+    b = b + 1;
+}
