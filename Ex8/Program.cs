8203be27163f5fa6a65954e8b2ea9874d913e46d
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-14 13:05:38 +0300

    Ex8

---
+++ Ex8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите максимальный индекс массива: ");  //Показать четные числа от 1 до N
+int a = int.Parse(Console.ReadLine());
+int count = 0;
+
+while(count < a)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(" ");
+        count = count + 1;
+    }
+    else 
+    {
+        count = count + 1;
+    }
+}
